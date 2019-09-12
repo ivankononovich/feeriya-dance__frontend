c5c657c67d83419db06e495049e718133b2f1495
Ivan Kononovich <fogfug1@gmail.com>
2019-09-12 21:34:50 +0300

    feat: add links to category menu

--- components/Categories/Categories.js
+++ components/Categories/Categories.js
@@ -1,18 +1,34 @@
+import Link from 'next/link';
+
+
 export default ({ categories }) => {
-    let renderContent = <li>Loading categories...</li>
+    let renderContent = <li>Loading categories...</li>;
 
     if (categories) {
-        renderContent = categories.map((item) => {
-            return <li key={ item.id }>
-                {item.nameRU}
-                <ul>
-                    {
-                        item.subcategory.map((item) => {
-                            return <li key={ item.id }>{ item.nameRU }</li>
-                        })
-                    }
-                </ul>
-            </li>
+        renderContent = categories.map((category) => {
+            const urlReq = '/category?';
+
+            return (
+                <li key={ category.id }>
+                    <Link href={ `${urlReq}id=${category.id}` }>
+                        <a>{ category.nameRU }</a>
+                    </Link>
+                    
+                    <ul>
+                        {
+                            category.subcategories.map((subcategory) => {
+                                return (
+                                    <li key={ subcategory.id }>
+                                        <Link href={ `${urlReq}id=${subcategory.id}` }>
+                                            <a>{ subcategory.nameRU }</a>
+                                        </Link>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </li>
+            );
         });
     }
 
