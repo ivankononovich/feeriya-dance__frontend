d0b83c12b441417c8647fdf6fc2a5aaae5ef2655
Ivan Kononovich <fogfug1@gmail.com>
2019-09-12 21:53:01 +0300

    feat: add a product get request on the categories page

--- pages/category.js
+++ pages/category.js
@@ -1,8 +1,25 @@
+import fetch from 'isomorphic-unfetch';
+
 import css from './../styles/category.scss';
 import Header from './../components/Header/Header';
 
 
-export default () => <>
-    <Header />
-    <h2 className={ css.title }>Category</h2>
-</>
+function CategoryPage({ products }) {
+    return <>
+        <Header />
+        <h2 className={ css.title }>Category</h2>
+    </>
+}
+
+CategoryPage.getInitialProps = async (ctx) => {
+    const protocol = ctx.req.connection.encrypted ? 'https://' : 'http://';
+
+    const req = await fetch(`${protocol}${ctx.req.headers.host}/api/content?name=products`);
+    const products = await req.json();
+
+    return {
+        products,
+    }
+}
+
+export default CategoryPage;
