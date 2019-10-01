c58c16a972a758412273c6a2c44f3acca31c6d46
Ivan Kononovich <ivan.kononovich.12@gmail.com>
2019-10-01 22:32:59 +0300

    feat: add link to basket page

--- components/MiniBasket/MiniBasket.js
+++ components/MiniBasket/MiniBasket.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 import css from './mini-basket.scss';
 
 
 export default ({ listProducts }) => 
-    <div className={ css['mini-basket'] }>
+    <Link href='/basket'>
+        <a className={ css['mini-basket'] }>
         <span className={ css['mini-basket__text'] }>
             Корзина: { listProducts.length ? listProducts.length : ' пустая' }
         </span>
-    </div>
+        </a>
+    </Link>
