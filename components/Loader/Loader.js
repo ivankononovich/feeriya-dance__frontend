85722f99a3f66abb8cbb07744fbca87392a97c05
Ivan Kononovich <ivan.kononovich.12@gmail.com>
2019-10-19 00:12:04 +0300

    feat: add loader component

---
+++ components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import css from './loader.scss';
+
+
+export default () => (
+    <div className={ css.loader }>
+        <span className={ css['loader__dot-move'] } />
+        <div className={ css['loader__dots-wrapper'] }>
+            <span className={ css.loader__dot } />
+            <span className={ css.loader__dot } />
+            <span className={ css.loader__dot } />
+        </div>
+    </div>
+)
